b92517ff800d4bf64627a07e6c4fa8e082a4bfd1
alphagammareal <sakyialpha2@gmail.com>
2025-11-08 17:53:15 +0000

    changes corrected

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -1,8 +1,8 @@
 from django.urls import path
-from . import views
+from .views import list_books, LibraryDetailView, index
 
 urlpatterns = [
-    path('', views.index, name='index'),  # Home page
-    path('books/', views.book_list, name='book_list'),  # Function-based view for listing all books
-    path('library/<int:pk>/', views.LibraryDetailView.as_view(), name='library_detail'),  # Class-based view for a specific library
+    path('', index, name='index'),  # Home page
+    path('books/', list_books, name='book_list'),  # Function-based view
+    path('library/<int:pk>/', LibraryDetailView.as_view(), name='library_detail'),  # Class-based view
 ]
