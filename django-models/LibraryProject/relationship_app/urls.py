cda18841d224adad8408ac06c1f3dadab02cab8d
alphagammareal <sakyialpha2@gmail.com>
2025-11-08 20:22:56 +0000

    changes correction

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -1,20 +1,13 @@
 from django.urls import path
-from .views import (
-    list_books,
-    LibraryDetailView,
-    index,
-    register,
-    UserLoginView,
-    UserLogoutView
-)
+from . import views
 
 urlpatterns = [
-    path('', index, name='index'),
-    path('books/', list_books, name='book_list'),
-    path('library/<int:pk>/', LibraryDetailView.as_view(), name='library_detail'),
+    path('', views.index, name='index'),
+    path('books/', views.list_books, name='book_list'),
+    path('library/<int:pk>/', views.LibraryDetailView.as_view(), name='library_detail'),
 
     # Authentication URLs
-    path('register/', register, name='register'),
-    path('login/', UserLoginView.as_view(template_name='relationship_app/login.html'), name='login'),
-    path('logout/', UserLogoutView.as_view(template_name='relationship_app/logout.html'), name='logout'),
+    path('register/', views.register, name='register'),
+    path('login/', views.UserLoginView.as_view(template_name='relationship_app/login.html'), name='login'),
+    path('logout/', views.UserLogoutView.as_view(template_name='relationship_app/logout.html'), name='logout'),
 ]
