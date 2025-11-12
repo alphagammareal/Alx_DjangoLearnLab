25008ef1d89eceee491605c91f4f37653a83ed66
alphagammareal <sakyialpha2@gmail.com>
2025-11-12 12:34:57 +0000

    custom user model creation

--- advanced_features_and_security/LibraryProject/bookshelf/admin.py
+++ advanced_features_and_security/LibraryProject/bookshelf/admin.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
 from .models import Book 
+from django.contrib.auth.admin import UserAdmin
+from .models import CustomUser
 
 # Register your models here.
 class BookAdmin(admin.ModelAdmin):
@@ -7,4 +9,21 @@ class BookAdmin(admin.ModelAdmin):
     list_filter = ('publication_year',)  # Filter sidebar
     search_fields = ('title', 'author') # Enables search by title or author
 
+# Register your models here.
+
+# Custom admin configuration
+class CustomUserAdmin(UserAdmin):
+    # Add the new fields to the admin panel
+    fieldsets = UserAdmin.fieldsets + (
+        (None, {'fields': ('date_of_birth', 'profile_picture')}),
+    )
+
+    add_fieldsets = UserAdmin.add_fieldsets + (
+        (None, {'fields': ('date_of_birth', 'profile_picture')}),
+    )
+
+    list_display = ('username', 'email', 'date_of_birth', 'is_staff', 'is_active')
+    search_fields = ('username', 'email')
+
+admin.site.register(CustomUser, CustomUserAdmin)
 admin.site.register(Book, BookAdmin)
