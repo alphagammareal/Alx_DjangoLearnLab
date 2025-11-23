c7cf01f9e3aaa4a17681680593e3d5d7280b6a5c
alphagammareal <sakyialpha2@gmail.com>
2025-11-23 12:21:48 +0000

    implement CRUD (Create, Read, Update, Delete) operations on the Book model.

--- api_project/api/views.py
+++ api_project/api/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from rest_framework import generics
+from rest_framework import generics, viewsets
 from .models import Book
 from .serializers import BookSerializer
 
@@ -7,6 +7,19 @@ from .serializers import BookSerializer
 class BookList(generics.ListAPIView):
     """
     GET /api/books/  -> returns list of all books
+    """
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+
+# NEW: Full CRUD using ModelViewSet
+class BookViewSet(viewsets.ModelViewSet):
+    """
+    Provides:
+    - GET /books_all/          (list)
+    - GET /books_all/<id>/     (retrieve)
+    - POST /books_all/         (create)
+    - PUT /books_all/<id>/     (update)
+    - DELETE /books_all/<id>/  (destroy)
     """
     queryset = Book.objects.all()
     serializer_class = BookSerializer
