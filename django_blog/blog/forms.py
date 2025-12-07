06fec9caae9e73fe0e50199b58885514e398ee61
alphagammareal <sakyialpha2@gmail.com>
2025-12-07 22:47:32 +0000

    changes made: corrections made to forms.py

--- django_blog/blog/forms.py
+++ django_blog/blog/forms.py
@@ -1,27 +1,24 @@
 from django import forms
 from .models import Post, Comment
 from django.contrib.auth.forms import UserCreationForm
-from django.contrib.auth.models import User
-from taggit.forms import TagField
+from taggit.forms import TagWidget
 
-
+# Post Form
 class PostForm(forms.ModelForm):
-    tags = TagField(required=False)  # use TagField for tags
-
     class Meta:
         model = Post
-        fields = ['title', 'content', 'excerpt', 'published_date', 'tags']
+        fields = ['title', 'content', 'tags']
+        widgets = {
+            'tags': TagWidget(attrs={'class': 'tag-input'}),
+        }
 
-
+# Comment Form
 class CommentForm(forms.ModelForm):
     class Meta:
         model = Comment
         fields = ['content']
 
-
+# Custom User Creation Form
 class CustomUserCreationForm(UserCreationForm):
-    email = forms.EmailField(required=True)
-
-    class Meta:
-        model = User
-        fields = ("username", "email", "password1", "password2")
+    class Meta(UserCreationForm.Meta):
+        fields = UserCreationForm.Meta.fields + ('email',)
