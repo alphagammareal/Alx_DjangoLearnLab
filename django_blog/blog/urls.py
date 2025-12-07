c1b7eb44c396159a94d3b994aef98e514e2fbdfc
alphagammareal <sakyialpha2@gmail.com>
2025-12-07 21:04:07 +0000

    CRUD: implemented CRUD operations

--- django_blog/blog/urls.py
+++ django_blog/blog/urls.py
@@ -5,14 +5,15 @@ from . import views
 app_name = 'blog'
 
 urlpatterns = [
-    path('', views.post_list, name='post_list'),
-    path('post/<int:pk>/', views.post_detail, name='post_detail'),
+    path('', views.PostListView.as_view(), name='post_list'),
+    path('post/<int:pk>/', views.PostDetailView.as_view(), name='post_detail'),
+    path('post/new/', views.PostCreateView.as_view(), name='post_create'),
+    path('post/<int:pk>/edit/', views.PostUpdateView.as_view(), name='post_update'),
+    path('post/<int:pk>/delete/', views.PostDeleteView.as_view(), name='post_delete'),
 
-    # registration and profile views
-    path('register/', views.register, name='register'),
-    path('profile/', views.profile, name='profile'),
-
-    # Django built-in login/logout
+    path('register/', views.register_view, name='register'),
+    path('profile/', views.profile_view, name='profile'),
+    
     path('login/', auth_views.LoginView.as_view(template_name='blog/login.html'), name='login'),
     path('logout/', auth_views.LogoutView.as_view(next_page='blog:post_list'), name='logout'),
 ]
--- django_blog/blog/views.py
+++ django_blog/blog/views.py
@@ -1,50 +1,99 @@
-from django.shortcuts import render, get_object_or_404, redirect
-from django.contrib.auth import login, authenticate
-from django.contrib.auth.decorators import login_required
+from django.shortcuts import render
+from django.shortcuts import get_object_or_404, redirect
+from django.urls import reverse_lazy, reverse
+from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
+from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
 from django.contrib import messages
-
 from .models import Post
-from .forms import CustomUserCreationForm
+from .forms import PostForm, CustomUserCreationForm
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.models import User
 
+# List and Detail (public)
+class PostListView(ListView):
+    model = Post
+    template_name = 'blog/post_list.html' 
+    context_object_name = 'posts'
+    paginate_by = 10
 
+class PostDetailView(DetailView):
+    model = Post
+    template_name = 'blog/post_detail.html'
+    context_object_name = 'post'
 
-# POST LIST
-def post_list(request):
-    posts = Post.objects.select_related('author').all()
-    return render(request, 'blog/post_list.html', {'posts': posts})
+# Create (authenticated users only)
+class PostCreateView(LoginRequiredMixin, CreateView):
+    model = Post
+    form_class = PostForm
+    template_name = 'blog/post_form.html'
 
+    def form_valid(self, form):
+        # set the author before saving
+        form.instance.author = self.request.user
+        response = super().form_valid(form)
+        messages.success(self.request, "Post created successfully.")
+        return response
 
-# POST DETAIL
-def post_detail(request, pk):
-    post = get_object_or_404(Post, pk=pk)
-    return render(request, 'blog/post_detail.html', {'post': post})
+    def get_success_url(self):
+        return reverse('blog:post_detail', kwargs={'pk': self.object.pk})
 
+# Update (only author)
+class PostUpdateView(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
+    model = Post
+    form_class = PostForm
+    template_name = 'blog/post_form.html'
 
-# USER REGISTRATION
-def register(request):
+    def test_func(self):
+        post = self.get_object()
+        return post.author == self.request.user
+
+    def handle_no_permission(self):
+        messages.error(self.request, "You do not have permission to edit this post.")
+        return redirect('blog:post_detail', pk=self.get_object().pk)
+
+    def form_valid(self, form):
+        messages.success(self.request, "Post updated successfully.")
+        return super().form_valid(form)
+
+    def get_success_url(self):
+        return reverse('blog:post_detail', kwargs={'pk': self.object.pk})
+
+# Delete (only author)
+class PostDeleteView(LoginRequiredMixin, UserPassesTestMixin, DeleteView):
+    model = Post
+    template_name = 'blog/post_confirm_delete.html'
+    success_url = reverse_lazy('blog:post_list')
+
+    def test_func(self):
+        post = self.get_object()
+        return post.author == self.request.user
+
+    def handle_no_permission(self):
+        messages.error(self.request, "You do not have permission to delete this post.")
+        return redirect('blog:post_detail', pk=self.get_object().pk)
+
+    def delete(self, request, *args, **kwargs):
+        messages.success(request, "Post deleted.")
+        return super().delete(request, *args, **kwargs)
+# REGISTER VIEW
+def register_view(request):
     if request.method == "POST":
         form = CustomUserCreationForm(request.POST)
         if form.is_valid():
-            user = form.save()
-            messages.success(request, "Registration successful!")
-            login(request, user)
-            return redirect('blog:post_list')
+            form.save()
+            messages.success(request, "Registration successful. You can now log in.")
+            return redirect("blog:login")
     else:
         form = CustomUserCreationForm()
-    return render(request, 'blog/register.html', {'form': form})
+    return render(request, "blog/register.html", {"form": form})
 
-
-
-
-# USER PROFILE
+# PROFILE VIEW
 @login_required
-def profile(request):
+def profile_view(request):
     if request.method == "POST":
-        request.user.email = request.POST.get("email", request.user.email)
+        new_email = request.POST.get("email")
+        request.user.email = new_email
         request.user.save()
-        messages.success(request, "Profile updated successfully!")
+        messages.success(request, "Profile updated successfully.")
         return redirect("blog:profile")
-
     return render(request, "blog/profile.html")
-
-
