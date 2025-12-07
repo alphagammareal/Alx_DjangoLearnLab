03e2a5cd7b8971d6acc4f926a3ae9157af105a6f
alphagammareal <sakyialpha2@gmail.com>
2025-12-07 21:07:15 +0000

    blog/urls: Url patterns Update url

--- django_blog/blog/urls.py
+++ django_blog/blog/urls.py
@@ -8,7 +8,7 @@ urlpatterns = [
     path('', views.PostListView.as_view(), name='post_list'),
     path('post/<int:pk>/', views.PostDetailView.as_view(), name='post_detail'),
     path('post/new/', views.PostCreateView.as_view(), name='post_create'),
-    path('post/<int:pk>/edit/', views.PostUpdateView.as_view(), name='post_update'),
+    path('post/<int:pk>/update/', views.PostUpdateView.as_view(), name='post_update'),
     path('post/<int:pk>/delete/', views.PostDeleteView.as_view(), name='post_delete'),
 
     path('register/', views.register_view, name='register'),
