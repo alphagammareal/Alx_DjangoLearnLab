af450dfcaa04cfcb520eb62da612d65058bdcd29
alphagammareal <sakyialpha2@gmail.com>
2025-12-07 20:39:12 +0000

    Changes mades to all files after some errors noted

--- django_blog/blog/urls.py
+++ django_blog/blog/urls.py
@@ -1,4 +1,5 @@
 from django.urls import path
+from django.contrib.auth import views as auth_views
 from . import views
 
 app_name = 'blog'
@@ -6,4 +7,12 @@ app_name = 'blog'
 urlpatterns = [
     path('', views.post_list, name='post_list'),
     path('post/<int:pk>/', views.post_detail, name='post_detail'),
+
+    # registration and profile views
+    path('register/', views.register, name='register'),
+    path('profile/', views.profile, name='profile'),
+
+    # Django built-in login/logout
+    path('login/', auth_views.LoginView.as_view(template_name='blog/login.html'), name='login'),
+    path('logout/', auth_views.LogoutView.as_view(next_page='blog:post_list'), name='logout'),
 ]
--- django_blog/blog/views.py
+++ django_blog/blog/views.py
@@ -1,14 +1,50 @@
-from django.shortcuts import render, get_object_or_404
-from .models import Post
+from django.shortcuts import render, get_object_or_404, redirect
+from django.contrib.auth import login, authenticate
+from django.contrib.auth.decorators import login_required
+from django.contrib import messages
 
-# Create your views here.
-from django.shortcuts import render, get_object_or_404
 from .models import Post
+from .forms import CustomUserCreationForm
 
+
+
+# POST LIST
 def post_list(request):
     posts = Post.objects.select_related('author').all()
     return render(request, 'blog/post_list.html', {'posts': posts})
 
+
+# POST DETAIL
 def post_detail(request, pk):
     post = get_object_or_404(Post, pk=pk)
     return render(request, 'blog/post_detail.html', {'post': post})
+
+
+# USER REGISTRATION
+def register(request):
+    if request.method == "POST":
+        form = CustomUserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            messages.success(request, "Registration successful!")
+            login(request, user)
+            return redirect('blog:post_list')
+    else:
+        form = CustomUserCreationForm()
+    return render(request, 'blog/register.html', {'form': form})
+
+
+
+
+# USER PROFILE
+@login_required
+def profile(request):
+    if request.method == "POST":
+        request.user.email = request.POST.get("email", request.user.email)
+        request.user.save()
+        messages.success(request, "Profile updated successfully!")
+        return redirect("blog:profile")
+
+    return render(request, "blog/profile.html")
+
+
