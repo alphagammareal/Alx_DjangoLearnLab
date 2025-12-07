267c2928d8c46a572f1d16ab71d38d34065e746d
alphagammareal <sakyialpha2@gmail.com>
2025-12-07 16:50:13 +0000

    First Blog Commit

--- django_blog/blog/models.py
+++ django_blog/blog/models.py
@@ -1,3 +1,19 @@
 from django.db import models
+from django.conf import settings
 
 # Create your models here.
+class Post(models.Model):
+    title = models.CharField(max_length=200)
+    content = models.TextField()
+    published_date = models.DateTimeField(auto_now_add=True)
+    author = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE,
+        related_name='posts'
+    )
+
+    class Meta:
+        ordering = ['published_date']
+
+    def __str__(self):
+        return self.title
