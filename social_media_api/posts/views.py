e405f606a1f5f588b70109d5d594a430808736b4
Alpha_Gamma2 <wargod410@gmail.com>
2025-12-15 18:24:32 +0000

    changes made

--- social_media_api/posts/views.py
+++ social_media_api/posts/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from rest_framework import viewsets, permissions, filters
+from rest_framework import viewsets, permissions, filters, generics
 from django_filters.rest_framework import DjangoFilterBackend
 
 from .models import Post, Comment
@@ -27,3 +27,13 @@ class CommentViewSet(viewsets.ModelViewSet):
 
     def perform_create(self, serializer):
         serializer.save(author=self.request.user)
+
+class FeedView(generics.ListAPIView):
+    serializer_class = PostSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+    def get_queryset(self):
+        following_users = self.request.user.following.all()
+        return Post.objects.filter(
+            author__in=following_users
+        ).order_by('-created_at')
