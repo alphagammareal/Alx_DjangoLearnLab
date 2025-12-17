6dc4f6d89a36791d8ada89b65ada82c227a8863a
Alpha_Gamma2 <wargod410@gmail.com>
2025-12-17 08:26:51 +0000

    changes made: urls.oy unlike_post endpoint added

--- social_media_api/posts/urls.py
+++ social_media_api/posts/urls.py
@@ -2,7 +2,7 @@ from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 from .views import PostViewSet, CommentViewSet
 from .views import FeedView
-from .views import like_post
+from .views import like_post, unlike_post
 
 router = DefaultRouter()
 router.register(r'posts', PostViewSet, basename='posts')
@@ -12,4 +12,5 @@ urlpatterns = [
     path('', include(router.urls)),
     path('feed/', FeedView.as_view(), name='user-feed'),
     path('posts/<int:pk>/like/', like_post, name='like-post'),
+    path('posts/<int:pk>/unlike/', unlike_post, name='unlike-post'),
 ]
