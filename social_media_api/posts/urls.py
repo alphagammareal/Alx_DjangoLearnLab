0e26c6906398e3c63621fe11dbeb1f1300551f09
Alpha_Gamma2 <wargod410@gmail.com>
2025-12-17 08:24:02 +0000

    changes made: urls.oy like_post endpoint added

--- social_media_api/posts/urls.py
+++ social_media_api/posts/urls.py
@@ -2,6 +2,7 @@ from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 from .views import PostViewSet, CommentViewSet
 from .views import FeedView
+from .views import like_post
 
 router = DefaultRouter()
 router.register(r'posts', PostViewSet, basename='posts')
@@ -10,4 +11,5 @@ router.register(r'comments', CommentViewSet, basename='comments')
 urlpatterns = [
     path('', include(router.urls)),
     path('feed/', FeedView.as_view(), name='user-feed'),
+    path('posts/<int:pk>/like/', like_post, name='like-post'),
 ]
